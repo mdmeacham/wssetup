d44d7ce72146824cf60d2353a5423dcb51871629
Mike Meacham <mdmeacham@gmail.com>
2024-06-21 13:09:49 -0700

    first commit

---
+++ wssetup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+
+update-alternatives --install /usr/bin/vi vi /opt/nvim-linux64/bin/nvim 20
+
+sudo apt -y install tmux
+echo 'test -z "$TMUX" && (tmux attach || tmux new-session)' >> ~/.bashrc
+
+# Install node
+wget -q -O - https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install 20
+
+# install yarn
+corepack enable
+
+yarn global add @quasar/cli
+echo "PATH=$PATH:$(yarn global bin)" >> ~/.bashrc
+
+
+# Install flameshot
+sudo apt -y install flameshot
+
+# 
